0da5a02e7842326d1c744503b48a943a9aa7189a
deepali-gaurkar <deepaligaurkar29@gmail.com>
2025-11-14 21:24:35 +0000

    Added Main.java

--- src/main/java/com/ferovinium/stockflow/Main.java
+++ src/main/java/com/ferovinium/stockflow/Main.java
@@ -1,4 +1,33 @@
 package com.ferovinium.stockflow;
 
-public class App {
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO FEROVINIUM STOCKFLOW APPLICATION");
+        System.out.println("Enter your command: eg. sell wine 1000");
+        Scanner sc = new Scanner(System.in);
+        Inventory inventory = new Inventory();
+
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine().trim();
+            if (line.isEmpty()) continue;
+
+            String[] p = line.split(" ");
+            if (p.length != 3) continue;
+
+            String cmd = p[0];
+            String sku = p[1];
+            int qty = Integer.parseInt(p[2]);
+
+            if (cmd.equals("sell")) {
+                inventory.sell(sku, qty);
+            } else if (cmd.equals("buy")) {
+                inventory.buy(sku, qty);
+            }
+
+            inventory.printAll();
+        }
+    }
 }
